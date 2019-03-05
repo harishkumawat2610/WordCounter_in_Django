f72b3c032670e56d71ff248c3a79e86a11f10f0a
HARISH KUMAWAT <39532868+harishkumawat2610@users.noreply.github.com>
2019-03-05 14:00:03 +0530

    delete url file

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import view
-
-urlpatterns = [
-    path('', view.homepage, name='home'),
-    path('count',view.count,name='count'),
-    path('about',view.about,name='about'),
-   
-]
